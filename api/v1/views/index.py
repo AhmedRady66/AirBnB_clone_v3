80226fbfb8ca0e71448e29919b1af88fcfa9f0a6
Ahmed Rady <rady20991@gmail.com>
2024-08-07 02:46:15 +0300

    add

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,25 @@
 """Create Flask app"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
-def get_status():
+def api_status():
     """return a JSON"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('stats')
+def get_stats():
+    """retrieve the number of each objects by type"""
+    stats = {
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User'),
+    }
+
+    return jsonify(stats)
