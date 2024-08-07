8366c8776b39d0ede25edf26178daf14ccebdb3b
Ahmed Rady <rady20991@gmail.com>
2024-08-07 04:57:51 +0300

    CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """Create Flask app"""
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
