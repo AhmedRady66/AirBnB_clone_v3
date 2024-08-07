a237dac078f74f0905c8841855578a136ca1f340
Ahmed Rady <rady20991@gmail.com>
2024-08-07 03:06:49 +0300

    add

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Create Flask app"""
-from flask import Flask, jsonify
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -11,17 +11,11 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def close_db(error):
-    """ close storage """
+    """ Close Storage """
     storage.close()
-
-
-@app.error_handler(404)
-def not_found(error):
-    """not found error message"""
-    return jsonify({"error": "NOT FOUND"}), 404
 
 
 if __name__ == "__main__":
     h = getenv('HBNB_API_HOST', '0.0.0.0')
     p = int(getenv('HBNB_API_PORT', 5000))
-    app.run(debug=True, host=h, port=p, threaded=True)
+    app.run(host=h, port=p, threaded=True)
